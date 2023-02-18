ede647b57e8811b5cda20f98cea54c2fb92ca93a
thiagojordao98 <thiagojordao98@gmail.com>
2023-02-18 09:08:51 -0300

    alteração do argumento inválido "-crypto"

--- projeto1-IaC/iac-1.sh
+++ projeto1-IaC/iac-1.sh
@@ -15,17 +15,17 @@ groupadd GRP_SEC
 
 echo "Usuários ..."
 
-useradd carlos -m -c "Carlos Silva" -s /bin/bash -p $(openssl passwd -crypt pass123) -G GRP_ADM
-useradd maria -m -c "Maria Silva" -s /bin/bash -p $(openssl passwd -crypt pass123) -G GRP_ADM
-useradd joao -m -c "Joao Silva" -s /bin/bash -p $(openssl passwd -crypt pass123) -G GRP_ADM
+useradd carlos -m -c "Carlos Silva" -s /bin/bash -p $(openssl passwd -6 pass123) -G GRP_ADM
+useradd maria -m -c "Maria Silva" -s /bin/bash -p $(openssl passwd -6 pass123) -G GRP_ADM
+useradd joao -m -c "Joao Silva" -s /bin/bash -p $(openssl passwd -6 pass123) -G GRP_ADM
 
-useradd debora -m -c "Debora Souza" -s /bin/bash -p $(openssl passwd -crypt pass123) -G GRP_VEN
-useradd sebastiao -m -c "Sebastiao Souza" -s /bin/bash -p $(openssl passwd -crypt pass123) -G GRP_VEN
-useradd roberto -m -c "Roberto Souza" -s /bin/bash -p $(openssl passwd -crypt pass123) -G GRP_VEN
+useradd debora -m -c "Debora Souza" -s /bin/bash -p $(openssl passwd -6 pass123) -G GRP_VEN
+useradd sebastiao -m -c "Sebastiao Souza" -s /bin/bash -p $(openssl passwd -6 pass123) -G GRP_VEN
+useradd roberto -m -c "Roberto Souza" -s /bin/bash -p $(openssl passwd -6 pass123) -G GRP_VEN
 
-useradd joana -m -c "Joana Almeida" -s /bin/bash -p $(openssl passwd -crypt pass123) -G GRP_SEC
-useradd ana -m -c "Ana Almeida" -s /bin/bash -p $(openssl passwd -crypt pass123) -G GRP_SEC
-useradd jose -m -c "Jose Almeida" -s /bin/bash -p $(openssl passwd -crypt pass123) -G GRP_SEC
+useradd joana -m -c "Joana Almeida" -s /bin/bash -p $(openssl passwd -6 pass123) -G GRP_SEC
+useradd ana -m -c "Ana Almeida" -s /bin/bash -p $(openssl passwd -6 pass123) -G GRP_SEC
+useradd jose -m -c "Jose Almeida" -s /bin/bash -p $(openssl passwd -6 pass123) -G GRP_SEC
 
 echo "Permissões dos diretórios ..."
 
